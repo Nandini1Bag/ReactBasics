7438883fafeed271da4d5623a476fede69157c24
nandini <nininandini@gmail.com>
2019-11-06 14:43:53 -0800

    Handling dynamic content by javascript way.

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -45,7 +45,19 @@ class App extends Component {
       cursor:'pointer'
     };
      
-
+let persons=null;
+if(this.state.showPersons){
+persons=(<div>
+  <Person 
+  name={this.state.persons[0].name} 
+  age={this.state.persons[0].age}/>
+  <Person 
+  name={this.state.persons[1].name} 
+  age={this.state.persons[1].age} 
+  changed={this.nameChangeHandler}
+  click={this.switchNameHandler.bind(this,'max')}>N my hobbies:racing</Person>
+  </div>);
+}
     return (
       //Only one root element can be added i.e div in jsx
       //donot add () in the function call i.e switchNameHandler (we only want to send a reference)
@@ -53,19 +65,7 @@ class App extends Component {
       <div className="App"> 
         <h1>Hi i m react app</h1>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {
-        this.state.showPersons ?
-        <div>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/>
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age} 
-        changed={this.nameChangeHandler}
-        click={this.switchNameHandler.bind(this,'max')}>N my hobbies:racing</Person>
-        </div>:null
-        }
+        {persons}
       </div>
     );
    //JSX:-
