d5fa59ca8f4074da3cccf062609e23a3452a2ec7
nandini <nininandini@gmail.com>
2019-11-18 10:43:33 -0800

    Using Radium for Media Queries.

--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
-return (
-    <div className="Person">
+    /* If screen size is more than 500px wide, set the width of <div> to 450px 
+    and set the background color to olive */
+    const style={
+     '@media (min-width : 500px)': {
+         width :'450px',
+         backgroundColor: 'olive'
+     },
+     '@media (min-width : 0px) and (max-width : 500px)': {
+        width :'150px',
+        backgroundColor: 'yellow'
+    }
+    };
+return (   
+    <div className="Person" style={style}>
         <p onClick={props.click}> I am {props.name} and i m {props.age} years old.</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
@@ -12,5 +25,5 @@ return (
 )
 }
 
-export default person;
+export default Radium(person);
 
