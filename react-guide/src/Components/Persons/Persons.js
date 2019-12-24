5102ee715ec049733fe6202023fbc4500cf83654
nandini <nininandini@gmail.com>
2019-12-24 10:53:12 -0800

    PureComponent instead of shouldComponentUpdate.

--- react-guide/src/Components/Persons/Persons.js
+++ react-guide/src/Components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React ,{Component}from 'react';
+import React ,{PureComponent}from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component{
+class Persons extends PureComponent{
         
 // static getDerivedStateFromProps(props,state){
 //         console.log('[Persons.js] getDerivedStateFromProps');
@@ -13,15 +13,22 @@ class Persons extends Component{
 //         console.log('[Persons.js] componentWillReceiveProps');      
 // }
 
-shouldComponentUpdate(nextProps,nextState){
-        console.log('[Persons.js] shouldComponentUpdate');   
-        if (nextProps.persons !== this.props.persons){
-                return true; 
-        }else{
-                return false;      
-        }
+//Note:-If more properties are to be checked then dont use this function,
+//instead use PureComponent. 
+
+// shouldComponentUpdate(nextProps,nextState){
+//         console.log('[Persons.js] shouldComponentUpdate');   
+//         if (
+//            nextProps.persons !== this.props.persons ||
+//            nextProps.clicked !== this.props.clicked ||
+//            nextProps.changed !== this.props.changed
+//            ) {
+//                 return true; 
+//         }else{
+//                 return false;      
+//         }
          
-}
+// }
 
 getSnapshotBeforeUpdate(prevProps,prevState){
         console.log('[Persons.js] getSnapshotBeforeUpdate');  
