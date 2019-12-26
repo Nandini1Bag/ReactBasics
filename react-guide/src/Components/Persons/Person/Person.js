6ed600ee66e919061a42586a45aee0a2793556af
nandini <nininandini@gmail.com>
2019-12-26 11:04:24 -0800

    React.Fragment.

--- react-guide/src/Components/Persons/Person/Person.js
+++ react-guide/src/Components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React ,{Component}from 'react';
+import React ,{Component,Fragment}from 'react';
 import Auxillary from '../../../hoc/Auxillary';
 import styled from 'styled-components';
 //import './Person.css';
@@ -17,17 +17,21 @@ const StyledDiv=styled.div`
     } 
 `;
 
+
+//Without an extra div element u can use a wrapper component i.e Auxillary 
+//or u can use array with comma separated elements
+//or u can use Fragment which work same as Auxillary component and it is a buildin component.
 class Person extends Component {
     render(){
         console.log('[Person.js] is rendering...')
         return (   
-            <Auxillary>
+            <Fragment>
                 <p onClick={this.props.click}> I am {this.props.name} and i m {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
                 <input type="text" 
                 onChange={this.props.changed} 
                 value={this.props.name}/>
-            </Auxillary>
+            </Fragment>
         
         )
     }
